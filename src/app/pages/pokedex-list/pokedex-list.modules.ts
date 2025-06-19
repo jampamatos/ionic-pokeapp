67c92609ae8611ccdc3e6c562e276a3171562aa4
João Paulo Coutinho de Matos <jp.coutm@gmail.com>
2025-06-19 13:56:40 -0300

    fix(pokedex): criar módulo da página PokedexListPage

---
+++ src/app/pages/pokedex-list/pokedex-list.modules.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
+import { PokedexListPage } from './pokedex-list.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PokedexListPage,
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, RouterModule.forChild(routes)],
+  declarations: [PokedexListPage],
+})
+export class PokedexListPageModule {}
